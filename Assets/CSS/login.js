c6a4a7e7413a2650f06899c5315aec6f4ce57214
oyayafelix <oyayafelix@gmail.com>
2022-05-18 12:26:58 +0300

    added alerts to the form function

--- Assets/CSS/login.js
+++ Assets/CSS/login.js
@@ -1,12 +1,15 @@
+this.setHomePage('http://www.mysite.com');
+
 function validate()
 {
     var email=document.getElementById("email").value
     var password=document.getElementById("password").value
 
-    if (email=="admin" && password=="user") {
-        alert("login sucessfully");
-        return false;
-       
+    if (email=="admin" && password=="admin") {
+        alert("login sucessfull");
+        //return false;
+        return window.location= window.location = "https://www.google.com/";
+        
 
 
 
@@ -16,3 +19,12 @@ function validate()
         
     }
 }
+const buttonClick = document.querySelector(".button");
+
+buttonClick.addEventListener("click",function(){
+   
+        window.location = "https://www.google.com/"
+        
+   
+    
+})
