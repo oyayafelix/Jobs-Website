aa9a95ec4923c5b6fed898dcc45b612c4cda7ec2
oyaya <32357696+oyayafelix@users.noreply.github.com>
2022-05-19 13:37:15 +0300

    Revert "derik otieno merge"

--- Assets/CSS/login.js
+++ Assets/CSS/login.js
@@ -6,7 +6,7 @@ function validate()
     var password=document.getElementById("password").value
 
     if (email=="admin"  && password=="admin") {
-        window.location= "home.html";
+        window.location= "https://google.com";
         alert("login sucessfull");
         return true;
        
